ff85533f0cc069c897f7be2764c4c46d38148c6e
JalemOG <janikhamilton1441@gmail.com>
2026-01-16 13:29:33 -0600

    Add placeholder Main for Client project

--- src/Calculator.Client/Program.cs
+++ src/Calculator.Client/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace Calculator.Client
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator.Client - placeholder (Sprint 3)");
+        }
+    }
+}
